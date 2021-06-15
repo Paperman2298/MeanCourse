47e768e6848bc35e278fef06ac75c3cc8b781820
JoelSantillan <joel7164@gmail.com>
2021-06-14 19:17:13 -0500

    Added angular material and header

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
- 
+
 @Component({
   selector: 'app-post-create',
-  templateUrl: './post-create.component.html'
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent implements OnInit {
   newPost = 'NO CONTENT';
