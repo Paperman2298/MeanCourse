f12ee56af6d4afbea43aff3180354e8f7419c82e
JoelSantillan <joel7164@gmail.com>
2021-06-15 16:12:12 -0500

    Adding form validation

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Post } from '../post.model';
 
 @Component({
@@ -16,10 +17,11 @@ export class PostCreateComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onAddPost = () => {
+  onAddPost = (form: NgForm) => {
+    if (form.invalid) { return; }
     const post: Post = {
-      title: this.enteredTitle,
-      content: this.enteredContent,
+      title: form.value.title,
+      content: form.value.content,
     };
 
     this.postCreated.emit(post);
