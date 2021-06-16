4117eb05097ee6878e4bd2d8c3f79323d6afdcea
JoelSantillan <joel7164@gmail.com>
2021-06-15 19:59:18 -0500

    Calling GET post with Subject rxjs

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,6 +1,6 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { Post } from '../post.model';
+import { PostsService } from '../post.service';
 
 @Component({
   selector: 'app-post-create',
@@ -10,21 +10,16 @@ import { Post } from '../post.model';
 export class PostCreateComponent implements OnInit {
   enteredTitle = '';
   enteredContent = '';
-  @Output() postCreated = new EventEmitter<Post>();
 
-  constructor() { }
+  constructor(public postsService: PostsService) { }
 
   ngOnInit(): void {
   }
 
   onAddPost = (form: NgForm) => {
     if (form.invalid) { return; }
-    const post: Post = {
-      title: form.value.title,
-      content: form.value.content,
-    };
 
-    this.postCreated.emit(post);
+    this.postsService.addPost(form.value.title, form.value.content);
   }
 
 }
