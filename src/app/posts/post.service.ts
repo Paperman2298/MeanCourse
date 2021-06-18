bf88dc82fcf8163f540b6060c4b0606d4658f761
JoelSantillan <joel7164@gmail.com>
2021-06-18 17:20:55 -0500

    Ending sectiong 4/Adding posts with an ID

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -39,9 +39,11 @@ export class PostsService {
             title,
             content
         };
-        this.http.post<{message: string}>('http://localhost:3000/api/posts', post)
+        this.http.post<{message: string, postId: string}>('http://localhost:3000/api/posts', post)
          .subscribe((responseData) => {
             console.log(responseData);
+            const id = responseData.postId;
+            post.id = id;
             this.posts.push(post);
             this.postsUpdated.next([...this.posts]);
          });
