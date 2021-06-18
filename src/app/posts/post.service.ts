0e613c5ff0370eb457119ae95096206ea7254fc3
JoelSantillan <joel7164@gmail.com>
2021-06-17 22:37:04 -0500

    Understanding CORS and posting from server

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { Post } from './post.model';
@@ -7,8 +8,14 @@ export class PostsService {
     private posts: Post[] = [];
     private postsUpdated = new Subject<Post[]>();
 
+    constructor(private http: HttpClient) { }
+
     getPosts = () => {
-        return [...this.posts];
+        this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
+         .subscribe((postData) => {
+            this.posts = postData.posts;
+            this.postsUpdated.next([...this.posts]);
+         });
     }
 
     getPostUpdateListener = () => {
@@ -17,6 +24,7 @@ export class PostsService {
 
     addPost = (title: string, content: string) => {
         const post: Post = {
+            id: null,
             title,
             content
         };
