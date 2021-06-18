930282b2687276376835e737e7969cce46993878
JoelSantillan <joel7164@gmail.com>
2021-06-17 22:52:44 -0500

    Finished Section 3

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
     next();
+});
+
+app.post('/api/posts', (req, res, next) => {
+    const posts = req.body;
+    console.log(posts);
+    res.status(201).json({
+        message: 'Post added successfully'
+    }); //Typical number to express everything is okay
 });
 
 app.use('/api/posts', (req, res, next) => {
