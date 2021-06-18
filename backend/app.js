795260af96b051f98351046dd45c4d8720c5b950
JoelSantillan <joel7164@gmail.com>
2021-06-18 16:35:28 -0500

    Fetching data from mongo

--- backend/app.js
+++ backend/app.js
@@ -34,25 +34,15 @@ app.post('/api/posts', (req, res, next) => {
     post.save();
     res.status(201).json({
         message: 'Post added successfully'
-    }); //Typical number to express everything is okay
+    });
 });
 
-app.use('/api/posts', (req, res, next) => {
-    const posts = [{
-            id: 'qw3d4fe56gr7',
-            title: 'First server-side post',
-            content: 'This is coming from the server'
-        },
-        {
-            id: 'csev5d4swd3xrr',
-            title: 'Second server-side post',
-            content: 'This is coming from the server! Wow!'
-        }
-    ];
-
-    res.status(200).json({
-        message: 'Posts fetched successfully!',
-        posts: posts
+app.get('/api/posts', (req, res, next) => {
+    Post.find().then(documents => {
+        res.status(200).json({
+            message: 'Posts fetched successfully!',
+            posts: documents
+        });
     });
 });
 
