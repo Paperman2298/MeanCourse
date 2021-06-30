8d6dc8453da4ee85356e23a1f7a22ecea0085555
JoelSantillan <joel7164@gmail.com>
2021-06-30 15:55:20 -0500

    Added the edit feature

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+
+import { Post } from '../post.model';
 import { PostsService } from '../post.service';
 
 @Component({
@@ -10,17 +13,34 @@ import { PostsService } from '../post.service';
 export class PostCreateComponent implements OnInit {
   enteredTitle = '';
   enteredContent = '';
+  private mode = 'create';
+  private postId: string;
+  public post: Post;
 
-  constructor(public postsService: PostsService) { }
+  constructor(public postsService: PostsService, public route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has('postId')){
+        this.mode = 'edit';
+        this.postId = paramMap.get('postId');
+        this.post = this.postsService.getPost(this.postId);
+      }else {
+        this.mode = 'create';
+        this.postId = null;
+      }
+    });
   }
 
-  onAddPost = (form: NgForm) => {
+  onSavePost = (form: NgForm) => {
     if (form.invalid) { return; }
 
-    this.postsService.addPost(form.value.title, form.value.content);
-    form.resetForm();
+    if (this.mode === 'create'){
+      this.postsService.addPost(form.value.title, form.value.content);
+      form.resetForm();
+    }else {
+      this.postsService.updatePost(this.postId, form.value.title, form.value.content);
+    }
   }
 
 }
--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -33,6 +33,10 @@ export class PostsService {
         return this.postsUpdated.asObservable();
     }
 
+    getPost = (id: string) => {
+        return {...this.posts.find(post => post.id === id)};
+    }
+
     addPost = (title: string, content: string) => {
         const post: Post = {
             id: null,
@@ -47,6 +51,13 @@ export class PostsService {
             this.posts.push(post);
             this.postsUpdated.next([...this.posts]);
          });
+    }
+
+    updatePost = (id: string, title: string, content: string) => {
+        const post: Post = { id, title, content };
+        this.http.put(`http://localhost:3000/api/posts/${id}`, post).subscribe(response => {
+            console.log(response);
+        });
     }
 
     deletePost = (postId: string) => {
